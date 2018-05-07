1a709e5d49cd8b81fb5a17eb9d3d6ebee0e922a1
Annie <annie.kellner@colostate.edu>
2018-05-07 13:57:27 -0600

    Added +300000 to X and Y coords to see whether NA/NaN error is caused by negative numbers. Does not appear to be the case.

--- land_data_explore.R
+++ land_data_explore.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(foreign)
 library(dplyr)
 library(tidyr)
@@ -5,6 +7,7 @@ library(adehabitatLT)
 library(lubridate)
 
 land <- read.dbf("C:/Users/akell/Desktop/Spring_2018/Research/GIS/land_locations.dbf") #Import attribute data from shapefile
+
 
 ### Concatenate and format time columns #####
 
@@ -20,6 +23,12 @@ str(land)
 land$datetime <- ymd_hms(paste(land$year, land$month, land$day, land$hour, land$minute, land$second, sep = "-")) #use lubridate fxn to format datetime, add column to land dataframe
 str(land)
 
+write.csv(land, file = "Land.csv")
+
+# Test to see whether error in adehabitat is beacuse of negative numbers
+land_trial <- mutate(land, x_new=X+300000, y_new=Y+300000)
+
+
 ### Individuals per year ###
 
 table(land$animal, land$year)
@@ -28,4 +37,5 @@ table(land$animal, land$year)
 tt<-paste(land$animal, land$datetime)
 table(duplicated(tt, fromLast=F)) #Test if you have duplicated data
 
-traj<-as.ltraj(xy=land[,X:Y], date=land$datetime, id=as.character(land$animal))
+traj<-as.ltraj(xy=land_trial[,"X":"Y"], date=land_trial$datetime, id=as.character(land_trial$animal))
+traj2<-ld(traj)
